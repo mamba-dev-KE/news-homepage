5ab7fcfe8cf3a5a18b074bea0c31818d59dbd903
Joseph Maramba <josephmaramba50@gmail.com>
2022-11-18 11:53:49 +0300

    position title

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@fontsource/inter/700.css';
+import '@fontsource/inter/800.css';
 
 import './styles/index.scss';
 import './styles/reset.scss';
