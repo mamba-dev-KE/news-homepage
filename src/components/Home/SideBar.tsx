6426312e5dbf5f0f3fec6a879743a8d6cb052cd9
Joseph Maramba <josephmaramba50@gmail.com>
2022-11-20 10:27:15 +0300

    styling side bar

--- src/components/Home/SideBar.tsx
+++ src/components/Home/SideBar.tsx
@@ -5,16 +5,16 @@ export const SideBar = () => {
 
   return (
     <aside className="sidebar">
-      <div className="sidebar-top">
+      <div className="sidebar-top sidebar-separator">
         <h2>{top.title}</h2>
         <h3>{top.subtitle}</h3>
         <p>{top.text}</p>
       </div>
-      <div className="sidebar-mid">
+      <div className="sidebar-mid sidebar-separator">
         <h3>{mid.title}</h3>
         <p>{mid.text}</p>
       </div>
-      <div className="sidebar-bottom">
+      <div className="sidebar-bottom sidebar-separator">
         <h3>{bottom.title}</h3>
         <p>{bottom.text}</p>
       </div>
